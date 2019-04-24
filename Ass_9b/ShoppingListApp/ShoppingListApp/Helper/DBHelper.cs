9f56d3074beaa9aef80560145f5e4a26b3f43360
Mark00reb <u17135983@tuks.co.za>
2019-04-25 00:08:14 +0200

    Initial Commit

---
+++ Ass_9b/ShoppingListApp/ShoppingListApp/Helper/DBHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Database;
+using Android.Database.Sqlite;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ShoppingListApp.Helper
+{
+    public class DBHelper : SQLiteOpenHelper
+    {
+
+        private static string DB_NAME = "EDMTDev";
+        private static int DB_VER = 1;
+        public static string DB_TABLE = "Task";
+        public static string DB_COLUMN = "TaskName";
+
+
+        public DBHelper(Context context):base(context, DB_NAME,null,DB_VER) { }
+
+        public override void OnCreate(SQLiteDatabase db)
+        {
+            string query = $"CREATE TABLE {DBHelper.DB_TABLE} (ID INTEGER PRIMARY KEY AUTOINCREMENT,{DBHelper.DB_COLUMN} TEXT NOT NULL) ;";
+            db.ExecSQL(query);
+
+        }
+
+        public override void OnUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+        {
+            string query = $"DELETE TABLE IF EXISTS {DB_TABLE}";
+            db.ExecSQL(query);
+            OnCreate(db);
+        }
+
+        public void InsertNewTask(String task)
+        {
+            SQLiteDatabase db = this.WritableDatabase;
+            ContentValues values = new ContentValues();
+            values.Put(DB_COLUMN, task);
+            db.InsertWithOnConflict(DB_TABLE, null, values, Android.Database.Sqlite.Conflict.Replace);
+            db.Close();
+
+        }
+
+        public void deleteTask(String task)
+        {
+            SQLiteDatabase db = this.WritableDatabase;
+            db.Delete(DB_TABLE, DB_COLUMN + "= ?", new string[] { task });
+            db.Close();
+        }
+
+        public List<string> getTaskList()
+        {
+            List<String> tasklist = new List<string>();
+            SQLiteDatabase db = this.ReadableDatabase;
+            ICursor cursor = db.Query(DB_TABLE, new string[] { DB_COLUMN }, null, null, null, null, null);
+            while (cursor.MoveToNext())
+            {
+                int index = cursor.GetColumnIndex(DB_COLUMN);
+                tasklist.Add(cursor.GetString(index));
+            }
+            return tasklist;
+        }
+
+    }
+}
---
+++ Ass_9b/ShoppingListApp/ShoppingListApp/MainActivity.cs
@@ -0,0 +1,79 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Views;
+using Android.Content;
+using System;
+using ShoppingListApp.Helper;
+using System.Collections.Generic;
+
+namespace ShoppingListApp
+{
+    [Activity(Label = "@string/app_name"/*, Theme = "@style/AppTheme"*/, MainLauncher = true, /*Icon = "@drawable/icon",*/ Theme = "@style/Theme.AppCompat.Light")]
+    public class MainActivity : AppCompatActivity
+    {
+        EditText taskEditText;
+        DBHelper DBHelper;
+        CustomAdapter adapter;
+        ListView lstTask;
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_item, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch(item.ItemId)
+            {
+                case Resource.Id.action_add:
+                    taskEditText = new EditText(this);
+                    Android.Support.V7.App.AlertDialog dialog = new Android.Support.V7.App.AlertDialog.Builder(this)
+                        .SetTitle("Add New Item")
+                        .SetMessage("What item would you like to add to your shopping list?")
+                        .SetView(taskEditText)
+                        .SetPositiveButton("Add", OkAction)
+                        .SetNegativeButton("Cancel", CancelAction)
+                        .Create();
+                    dialog.Show();
+                    return true;
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void CancelAction(object sender, DialogClickEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OkAction(object sender, DialogClickEventArgs e)
+        {
+            string task = taskEditText.Text;
+            DBHelper.InsertNewTask(task);
+            LoadTaskList();
+        }
+
+        public void LoadTaskList()
+        {
+            List<string> taskList = DBHelper.getTaskList();
+            adapter = new CustomAdapter(this, taskList,DBHelper);
+            lstTask.Adapter = adapter;
+
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.activity_main);
+
+            DBHelper = new DBHelper(this);
+           lstTask = FindViewById<ListView>(Resource.Id.lstTask);
+
+            //Load Data
+            LoadTaskList();
+        }
+    }
+}
